af5794edddd3379d5753a262980f33a1207d20bf
cx.li <chunxialims@163.com>
2021-04-05 19:34:37 +0800

    exchanger threadpool

--- src/main/java/com/example/concurrency/juc/SemaphoreDemo.java
+++ src/main/java/com/example/concurrency/juc/SemaphoreDemo.java
@@ -3,6 +3,7 @@ package com.example.concurrency.juc;
 import java.util.concurrent.Semaphore;
 
 class MyThreadSem extends Thread {
+
     private Semaphore semaphore;
 
     public MyThreadSem(String name, Semaphore semaphore) {
@@ -24,12 +25,15 @@ class MyThreadSem extends Thread {
             System.out.println(Thread.currentThread().getName() + " release successfully");
         }
     }
+
 }
 
 public class SemaphoreDemo {
+
     public final static int SEM_SIZE = 10;
 
     public static void main(String[] args) {
+
         Semaphore semaphore = new Semaphore(SEM_SIZE);
         MyThreadSem t1 = new MyThreadSem("t1", semaphore);
         MyThreadSem t2 = new MyThreadSem("t2", semaphore);
@@ -47,5 +51,20 @@ public class SemaphoreDemo {
             semaphore.release();
             System.out.println(Thread.currentThread().getName() + " release successfully");
         }
+
+        // release一次就加一次令牌
+        semaphore = new Semaphore(2);
+        int availablePermits = semaphore.availablePermits();
+        System.out.println(availablePermits);
+        try {
+            semaphore.acquire(2);
+            System.out.println(semaphore.availablePermits());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        semaphore.release(); // 释放一个许可
+        semaphore.release();
+        System.out.println(semaphore.availablePermits());
     }
+
 }
